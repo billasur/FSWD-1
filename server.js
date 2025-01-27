7f5a39034214d02c22315fca7de5eaf7c12351d7
Kiesha Maria <kiesha.maria@kalvium.community>
2025-01-27 12:58:20 +0530

    completed CA

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ catch(err){
                 {
                     return res.status(404).json({message: "Restaurant not found"});
                 }
+              res.json(deletedRest);
             }
             catch(err){
                 res.status(500).json({message:err.message});
